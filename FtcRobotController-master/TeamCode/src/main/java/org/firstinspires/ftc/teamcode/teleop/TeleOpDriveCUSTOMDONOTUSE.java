401e6fa9c816cc595612c90f5895a73fe3bf0b09
NDRoboKnightsFTC <89369554+NDRoboKnightsFTC@users.noreply.github.com>
2021-10-14 15:32:19 -0700

    First commit

---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpDriveCUSTOMDONOTUSE.java
@@ -0,0 +1,107 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.ftcdevcommon.RobotLogCommon;
+import org.firstinspires.ftc.ftcdevcommon.android.WorkingDirectory;
+import org.firstinspires.ftc.teamcode.ProgrammingBoardHardware;
+import org.firstinspires.ftc.teamcode.common.FTCErrorHandling;
+import org.firstinspires.ftc.teamcode.common.RobotConstants;
+import org.firstinspires.ftc.teamcode.teleop.common.FTCButton;
+
+import com.qualcomm.hardware.rev.RevTouchSensor;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+
+@TeleOp(name = "CUSTOM TeleOp Drive", group = "Drive")
+//@Disabled
+public class TeleOpDriveCUSTOMDONOTUSE extends LinearOpMode {
+
+    private static final String TAG = "TeleOpDrive "; // logging tag identifier
+
+    private ProgrammingBoardHardware robot;
+    private final FTCButton rightBumper = new FTCButton();
+    private final FTCButton leftBumper = new FTCButton();
+
+    RevTouchSensor digitalTouch;  // Hardware Device Object
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        telemetry.addData("Initializing...", "Please wait until complete");
+        telemetry.update();
+
+        try {
+            RobotLogCommon.initialize(WorkingDirectory.getWorkingDirectory() + RobotConstants.logDir);
+            robot = new ProgrammingBoardHardware(hardwareMap);
+            robot.leftFrontMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+            telemetry.addData("Initialized!", "Ready to run");
+            telemetry.update();
+
+            digitalTouch = hardwareMap.get(RevTouchSensor.class, "digital_touch");
+
+            waitForStart(); // wait for the driver to push "Start"
+
+            while (opModeIsActive()) {
+                updateButtons();
+                updatePlayerOne();
+
+                if (digitalTouch.isPressed()) {
+                    telemetry.addData("Digital Touch", "Is Pressed");
+                } else {
+                    telemetry.addData("Digital Touch", "Is Not Pressed");
+                }
+
+                telemetry.update();
+            }
+        } catch (Exception ex) {
+            FTCErrorHandling.handleFtcErrors(ex, TAG, this);
+        } finally {
+            RobotLogCommon.closeLog();
+        }
+    }
+
+    // Update the state of the active buttons. This method should be
+    // called once per cycle.
+    private void updateButtons() {
+        rightBumper.update(gamepad1.right_bumper);
+        leftBumper.update(gamepad1.left_bumper);
+    }
+
+    // Execute the action(s) controlled by Player 1. This method
+    // should be called once per cycle.
+    private void updatePlayerOne() {
+        checkLeftBumper();
+        checkRightBumper();
+        checkBothBumperNotHeld();
+
+    }
+
+    private void checkBothBumperNotHeld() {
+        if (!leftBumper.is(FTCButton.State.HELD) && !rightBumper.is(FTCButton.State.HELD)) {
+            robot.leftFrontMotor.setPower(0.0);
+        }
+    }
+
+    private void checkLeftBumper() {
+        if (leftBumper.is(FTCButton.State.HELD)) {
+            robot.leftFrontMotor.setPower(-0.5);
+            telemetry.addData("Driving", "Left front reverse");
+            telemetry.update();
+        }
+    }
+
+    private void checkRightBumper() {
+        if (rightBumper.is(FTCButton.State.HELD)) {
+            robot.leftFrontMotor.setPower(0.5);
+            telemetry.addData("Driving", "Left front forward");
+            telemetry.update();
+        }
+    }
+}
+
