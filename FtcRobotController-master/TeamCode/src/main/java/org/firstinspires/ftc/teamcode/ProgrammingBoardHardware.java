fe0badf90a3b4d55d81414e22d99ba8b6f383c1a
NDRoboKnightsFTC <89369554+NDRoboKnightsFTC@users.noreply.github.com>
2021-10-14 16:16:30 -0700

    Synchronized projects

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ProgrammingBoardHardware.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ProgrammingBoardHardware.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.rev.RevTouchSensor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -9,24 +10,28 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
 
 // Common hardware definitions for Autonomous and TeleOp.
 public class ProgrammingBoardHardware {
-      
+
     public final HardwareMap hwMap;
     public final DcMotor leftFrontMotor;
+    public final RevTouchSensor digitalTouch;
     //**TODO public final Servo basicServo;
     //**TODO public final ColorSensor colorSensor;
 
     public ProgrammingBoardHardware(HardwareMap hwm) {
         hwMap = hwm;
-   
+
         // Motor
         leftFrontMotor = hwMap.get(DcMotor.class, "lf");
         leftFrontMotor.setDirection(DcMotor.Direction.REVERSE);
         leftFrontMotor.setPower(0);
         leftFrontMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
- 
+
+        // Touch Sensor
+        digitalTouch = hwMap.get(RevTouchSensor.class, "digital_touch");
+
         // Servo
         //**TODO basicServo = hwMap.get(Servo.class, "basic_servo");
-    
+
         // Color sensor
         //**TODO colorSensor = hwMap.get(ColorSensor.class, "color_sensor");
     }
